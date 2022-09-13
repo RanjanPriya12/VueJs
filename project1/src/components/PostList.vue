30a98ed79da37221ea0762e64cdc034db322f0a9
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2022-09-13 16:59:31 +0530

    commit

---
+++ project1/src/components/PostList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+<button @click="getPots">Load Post</button>
+<h3 v-if="errorMsg">{{errorMsg}}</h3>
+<div v-for="item in post" :key="item.id">
+<p>{{item.title}}</p>
+    </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+    export default {
+        name:"PostList",
+        data(){
+      return{
+        post:[],
+        errorMsg:""
+      }
+    },
+    methods:{
+        getPots(){
+            axios.get(`https://jsonplaceholder.typicode.com/posts`)
+            .then((res)=>{
+                console.log(res.data);
+                this.post=res.data;
+            })
+            .catch(err=>{
+                console.log(err);
+                this.errorMsg='Error in retrieving data'
+            })
+        }
+    }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ project1/src/components/UserForm.vue
@@ -0,0 +1,88 @@
+<template>
+    <form @submit="formSubmit">
+<div>
+    <label for="name">Name</label>
+    <input type="text" id="name" v-model="formValues.name">
+  </div>
+  <div>
+    <label for="email">Email</label>
+    <input type="email" id="email" v-model="formValues.email">
+  </div>
+  <div>
+    <label for="profile">Profile Summary</label>
+    <textarea name="profileSummary" id="profile" cols="30" rows="10" v-model="formValues.profileSummary"/>
+  </div>
+  <div>
+    <label for="country">Select Your Country</label>
+    <select name="country" id="country" v-model="formValues.country" >
+      <option value="">Select country</option>
+      <option value="India">India</option>
+      <option value="USA">USA</option>
+      <option value="Singapore">Singapore</option>
+    </select>
+  </div>
+
+  <div>
+    <label for="job-location">Job Location</label>
+    <select name="job-location" id="job-location" multiple v-model="formValues.jobLocation" >
+      <option value="India">India</option>
+      <option value="USA">USA</option>
+      <option value="Singapore">Singapore</option>
+    </select>
+  </div>
+
+  <div>
+    <label for="gender">Select your Gender</label>
+    <input type="radio" name="gender" id="gender" v-model="formValues.gender">
+    <label for="">Male</label>
+    <input type="radio" name="gender" id="gender" v-model="formValues.gender">
+    <label for="">Female</label>
+  </div>
+
+  <div>
+    <label>What are your skills?</label>
+    <div>
+      <input type="checkbox" id="html" v-model="formValues.skills">
+      <label for="html">HTML</label>
+      <input type="checkbox" id="css" v-model="formValues.skills">
+      <label for="css">CSS</label>
+      <input type="checkbox" id="javaScript" v-model="formValues.skills">
+      <label for="javaScript">javaScript</label>
+    </div>
+  </div>
+  <div>
+    <button>Submit</button>
+  </div>
+</form>
+</template>
+
+<script>
+export default {
+   name:"UserForm",
+    data(){
+      return{
+        formValues:{
+          name:'',
+          email:'',
+          profileSummary:'',
+          country:'',
+          jobLocation:[],
+          gender:'',
+          skills:[]
+        }
+      }
+    },
+
+  methods:{
+    formSubmit(event){
+      event.preventDefault();
+      console.log('formData',this.formValues);
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
